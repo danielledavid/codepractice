c234ff5002c34c3599cd7cc7c614c49bc3e1301b
DANIELLE DAVID <60359055+danielledavid@users.noreply.github.com>
2025-08-27 12:48:35 -0400

    Time: 1199 ms (47.08%) | Memory: 0B (100.00%) - LeetSync

--- 1174-sales-analysis-iii/sales-analysis-iii.sql
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -1,11 +1,7 @@
 # Write your MySQL query statement below
-WITH filtered AS(
-    SELECT product_id
-    FROM SALES
-    WHERE sale_date < "2019-01-01" OR sale_date > "2019-03-31"
-)
-
-SELECT product_id, product_name
-FROM Product
-WHERE product_id NOT IN (SELECT product_id FROM filtered)
-AND product_id IN(SELECT product_id from SALES)
+SELECT p.product_id, p.product_name
+FROM Product p
+JOIN Sales s ON p.product_id = s.product_id
+GROUP BY p.product_id, p.product_name
+HAVING MIN(s.sale_date) >= '2019-01-01'
+   AND MAX(s.sale_date) <= '2019-03-31';
