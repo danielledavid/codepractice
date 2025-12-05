8291f6081f671b38a2276151453215827d75cd1f
DANIELLE DAVID <60359055+danielledavid@users.noreply.github.com>
2025-12-05 15:27:28 -0500

    Time: 257 ms (84.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 3554-premier-league-table-ranking/premier-league-table-ranking.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT team_id, team_name,
+(wins * 3) + draws AS points,
+RANK() OVER(ORDER BY ((wins * 3) + draws) desc) AS position
+FROM TeamStats
+ORDER BY points DESC, team_name
