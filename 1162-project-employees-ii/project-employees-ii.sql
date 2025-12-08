7d55dd227fb265cf7c5db233a976c4b2870e28a1
DANIELLE DAVID <60359055+danielledavid@users.noreply.github.com>
2025-12-08 15:58:24 -0500

    Time: 739 ms (51.12%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1162-project-employees-ii/project-employees-ii.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+WITH counts AS(
+    SELECT project_id, COUNT(project_id) AS project_count,
+    RANK() OVER(ORDER BY COUNT(project_id) DESC) AS ranked_projects
+    FROM Project
+    GROUP BY project_id
+)
+SELECT project_id
+FROM counts
+WHERE ranked_projects = 1;
