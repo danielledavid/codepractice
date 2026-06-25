fa59545a7685be066715f01dc3772f2317474aa5
DANIELLE DAVID <60359055+danielledavid@users.noreply.github.com>
2026-06-25 13:26:16 +0800

    Time: 369 ms (89.68%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-SELECT email as Email
+SELECT 
+email AS Email
 FROM Person
-GROUP BY Email
-HAVING COUNT(email) > 1;
+GROUP BY email
+HAVING COUNT(email) >1
