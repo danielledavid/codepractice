be2b10373f904893d2a5a784adf43c50679a3844
DANIELLE DAVID <60359055+danielledavid@users.noreply.github.com>
2026-06-25 15:15:06 +0800

    Time: 517 ms (99.93%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,7 +1,10 @@
 # Write your MySQL query statement below
 WITH duplicates AS(
-SELECT id, email, RANK() OVER (PARTITION BY email ORDER BY id) AS rnk
-FROM PERSON)
+    SELECT 
+    id,
+    ROW_NUMBER() OVER (PARTITION BY email ORDER BY id) AS ranked_emails
+    FROM Person
+)
 
 DELETE FROM Person
-WHERE id IN (SELECT id FROM duplicates WHERE rnk > 1)
+WHERE id NOT IN (SELECT id FROM duplicates WHERE ranked_emails = 1)
