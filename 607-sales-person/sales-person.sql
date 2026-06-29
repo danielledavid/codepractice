5fd32d4387d76dba65ccfa63599ac76ec7a80a7f
DANIELLE DAVID <60359055+danielledavid@users.noreply.github.com>
2026-06-29 14:06:07 +0800

    Time: 1763 ms (12.22%) | Memory: 0B (100.00%) - LeetSync

--- 607-sales-person/sales-person.sql
+++ 607-sales-person/sales-person.sql
@@ -1,14 +1,4 @@
 # Write your MySQL query statement below
-WITH red AS(
-    SELECT s.name as name
-    FROM SalesPerson as s
-    JOIN Orders as o
-    ON s.sales_id = o.sales_id
-    JOIN company as c
-    ON c.com_id = o.com_id
-    WHERE c.name = "RED"
-)
-
 SELECT name
-FROM SalesPerson as s
-WHERE s.name NOT IN (SELECT name FROM red)
+FROM SalesPerson s
+WHERE sales_id NOT IN (SELECT sales_id FROM Orders  NATURAL JOIN Company WHERE name = "RED")
