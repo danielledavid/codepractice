72cfc13aaa73cb5f5fc571e07aeba4a94b25f7a7
DANIELLE DAVID <60359055+danielledavid@users.noreply.github.com>
2026-06-29 14:44:03 +0800

    Time: 344 ms (98.93%) | Memory: 0B (100.00%) - LeetSync

--- 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
+++ 1136-actors-and-directors-who-cooperated-at-least-three-times/actors-and-directors-who-cooperated-at-least-three-times.sql
@@ -1,6 +1,10 @@
 # Write your MySQL query statement below
+WITH RANKED AS(
+    SELECT 
+    actor_id, director_id,
+    ROW_NUMBER() OVER (PARTITION BY actor_id, director_id ORDER BY director_id) AS ranked
+    FROM ActorDirector 
+)
 SELECT actor_id, director_id
-FROM
-ActorDirector
-GROUP BY actor_id, director_id
-HAVING count(director_id) >= 3
+FROM ranked 
+WHERE ranked = 3
