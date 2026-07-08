8361aedaf8ec1e27c89fba4ac7ae823c1a444f75
DANIELLE DAVID <60359055+danielledavid@users.noreply.github.com>
2026-07-09 02:07:02 +0800

    Time: 1661 ms (7.04%) | Memory: 0B (100.00%) - LeetSync

--- 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/replace-employee-id-with-the-unique-identifier.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-SELECT unique_id, name 
+SELECT unique_id, name
 FROM EmployeeUNI u
-RIGHT JOIN Employees e
+right JOIN Employees e
 ON e.id = u.id
+GROUP BY e.id
