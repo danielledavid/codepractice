525ad8193f88eef2e4ee30be1f865467927b8da2
DANIELLE DAVID <60359055+danielledavid@users.noreply.github.com>
2026-07-15 23:44:16 +0800

    Time: 955 ms (27.73%) | Memory: 0B (100.00%) - LeetSync

--- 1541-top-travellers/top-travellers.sql
+++ 1541-top-travellers/top-travellers.sql
@@ -1,9 +1,10 @@
 # Write your MySQL query statement below
-SELECT name, 
-CASE WHEN sum(distance) > 1 then SUM(distance)
-ELSE 0 END AS travelled_distance
-FROM Users
-LEFT JOIN RIDES
-ON USERS.ID = RIDES.USER_ID
-GROUP BY USERS.ID
-ORDER BY travelled_distance DESC, NAME 
+Select name, 
+CASE WHEN SUM(distance) IS NOT NULL THEN SUM(distance)
+ELSE 0
+END AS travelled_distance
+FROM Rides r
+RIGHT JOIN Users u
+ON r.user_id = u.id
+GROUP BY user_id
+ORDER BY travelled_distance DESC, name
