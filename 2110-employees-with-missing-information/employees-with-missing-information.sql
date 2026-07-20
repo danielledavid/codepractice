df5427824c91af8ff8a06cb7bcb41998a3882cc7
DANIELLE DAVID <60359055+danielledavid@users.noreply.github.com>
2026-07-20 15:39:15 -0400

    Time: 500 ms (99.11%) | Memory: 0B (100.00%) - LeetSync

--- 2110-employees-with-missing-information/employees-with-missing-information.sql
+++ 2110-employees-with-missing-information/employees-with-missing-information.sql
@@ -1,11 +1,17 @@
 # Write your MySQL query statement below
-SELECT employee_id
-FROM Employees
-WHERE employee_id NOT IN (Select employee_id FROM Salaries)
+WITH all_id AS(
+    SELECT employee_id 
+    FROM Employees
+    UNION 
+    SELECT employee_id
+    FROM Salaries
+)
+SELECT a.employee_id
+FROM all_id a
 
-UNION 
-
-SELECT employee_id
-FROM Salaries
-WHERE employee_id NOT IN (Select employee_id FROM Employees)
-ORDER BY employee_id
+LEFT JOIN Employees e
+ON a.employee_id = e.employee_id
+LEFT JOIN Salaries s
+ON a.employee_id = s.employee_id
+WHERE name IS NULL OR salary IS NULL
+ORDER BY a.employee_id
