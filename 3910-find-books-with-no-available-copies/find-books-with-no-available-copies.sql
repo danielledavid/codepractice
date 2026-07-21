458c65edfa2dfbd7464f67b92e690c5e77edda92
DANIELLE DAVID <60359055+danielledavid@users.noreply.github.com>
2026-07-20 23:52:16 -0400

    Time: 563 ms (48.68%) | Memory: 0B (100.00%) - LeetSync

--- 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
+++ 3910-find-books-with-no-available-copies/find-books-with-no-available-copies.sql
@@ -1,13 +1,13 @@
 # Write your MySQL query statement below
 WITH borrowed AS(
-    SELECT book_id, COUNT(book_id) AS current_borrowers, return_date
+    SELECT book_id, COUNT(book_id) AS current_borrowers
     FROM borrowing_records
     WHERE return_date IS NULL
     GROUP BY book_id
 )
-SELECT l.book_id, l.title, l.author, l.genre, l.publication_year, current_borrowers
+SELECT l.book_id, title, author, genre, publication_year, current_borrowers
 FROM library_books l
-RIGHT JOIN borrowed b
+LEFT JOIN borrowed b 
 ON l.book_id = b.book_id
-WHERE (total_copies - current_borrowers) = 0
-ORDER BY current_borrowers DESC, l.title ASC
+WHERE total_copies - current_borrowers = 0
+ORDER BY current_borrowers DESC, title
