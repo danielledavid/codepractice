2ccb23dcd768a07595acaa32c83ca9d8ba2527a4
DANIELLE DAVID <60359055+danielledavid@users.noreply.github.com>
2026-07-23 19:44:14 -0400

    Time: 741 ms (23.02%) | Memory: 0B (100.00%) - LeetSync

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-
-SELECT user_id, max(time_stamp) AS last_stamp
-from logins
-WHERE EXTrACT(YEAr from time_stamp) = 2020
-group by user_Id
+SELECT user_id,
+MAX(time_stamp) AS last_stamp
+FROM Logins
+WHERE YEAR(time_stamp) = '2020'
+GROUP BY user_id
