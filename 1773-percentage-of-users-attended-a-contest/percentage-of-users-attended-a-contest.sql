c9a63a04097d53c2cf8e7c2e7a3a07186e04784b
DANIELLE DAVID <60359055+danielledavid@users.noreply.github.com>
2026-07-24 17:54:14 -0400

    Time: 939 ms (80.79%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,11 +1,12 @@
 # Write your MySQL query statement below
-WITH SUMall AS(
-    SELECT COUNT(*) AS all_users
-    from Users
+WITH counted AS(
+    SELECT COUNT(user_id) as counts
+    FROM Users
 )
 SELECT contest_id,
-ROUND(count(r.user_id)/all_users * 100,2) AS percentage
-FROM REGISTER r
-CROSS JOIN SUMall
+ROUND((COUNT(user_id) / counts) * 100,2) AS percentage
+FROM Register
+CROSS JOIN counted
+
 GROUP BY contest_id
 ORDER BY percentage DESC, contest_id
