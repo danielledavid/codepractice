bbb7783530e4949ce2ae86b7becd440c6ef0dc64
DANIELLE DAVID <60359055+danielledavid@users.noreply.github.com>
2026-08-13 10:12:34 -0400

    Time: 546 ms (24.09%) | Memory: 0B (100.00%) - LeetSync

--- 3803-find-products-with-valid-serial-numbers/find-products-with-valid-serial-numbers.sql
+++ 3803-find-products-with-valid-serial-numbers/find-products-with-valid-serial-numbers.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-SELECT product_id, product_name, description
+SELECT *
 FROM products
-WHERE description COLLATE utf8mb3_bin REGEXP '\\bSN[0-9]{4}-[0-9]{4}\\b'
+WHERE REGEXP_LIKE(description, '\\bSN[0-9]{4}-[0-9]{4}\\b', 'c')
+ORDER BY product_id
